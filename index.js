13edce7f8b754d43b97e1c1813c26dd52e4b4d15
raza1315 <razasfs2003@gmail.com>
2025-01-31 14:25:17 +0530

    basic express app setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const fs=require("fs");
+const status=require("express-status-monitor");
+
+const app =express();
+const port=7000;
+app.use(status());
+
+
+app.get("/",(req,res)=>{
+
+})
+
+
+app.listen(port,()=>{
+console.log("server is running on port 7000");
+})
