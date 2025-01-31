55919061dfcc12feb7cc62523f16decfd2e8b06e
raza1315 <razasfs2003@gmail.com>
2025-01-31 15:11:56 +0530

    zip pipeline done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const fs=require("fs");
 const status=require("express-status-monitor");
+const zlib=require("zlib");
 
 const app =express();
 const port=7000;
@@ -21,7 +22,13 @@ res.write(chunk);
 stream.on("end",()=>{res.end();})
 })
 
+const makeZip=async(filePath,destinationPath)=>{
+const stream=fs.createReadStream(`${filePath}`,"utf-8");
+stream.pipe(zlib.createGzip().pipe(fs.createWriteStream(`${destinationPath}`)));
+}
 
+//calling the function to create zip file:
+makeZip('./plain.txt',"./plainsamplezip.zip");
 
 app.listen(port,()=>{
 console.log("server is running on port 7000");
