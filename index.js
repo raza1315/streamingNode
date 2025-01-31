595ba38730ed83aa238585d196bcb3fc0ecb59d1
raza1315 <razasfs2003@gmail.com>
2025-01-31 15:00:55 +0530

    streaming for readfile done

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ res.end(data);
 })
 })
 
+app.get("/chunks",(req,res)=>{
+const stream=fs.createReadStream("./plain.txt","utf-8");
+stream.on("data",(chunk)=>{
+res.write(chunk);
+})
+stream.on("end",()=>{res.end();})
+})
+
+
 
 app.listen(port,()=>{
 console.log("server is running on port 7000");
